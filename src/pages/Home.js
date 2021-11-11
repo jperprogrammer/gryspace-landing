fcfb89224ad4bc3157226000aa0c95c6a98f1211
jperprogrammer <jperprogrammer@gmail.com>
2021-11-10 22:23:11 -0500

    changed the animation of images smoothly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -65,9 +65,21 @@ const Home = () => {
     const canvasRef = useRef(null)
     let canvas
     let context
-    let camera
     let imagesParam = []
-    let w, h, randX, randY, mouseX, mouseY
+    let w, h, randX, randY
+
+    let mouse = {
+        x : window.innerWidth / 2,
+        y : window.innerHeight / 2,
+        w : 0,
+        alt : false,
+        shift : false,
+        ctrl : false,
+        buttonLastRaw : 0, // user modified value
+        buttonRaw : 0,
+        over : false,
+        buttons : [1, 2, 4, 6, 5, 3], // masks for setting and clearing button raw bits;
+    };
 
     useEffect(() => {
         canvas = canvasRef.current
@@ -75,12 +87,6 @@ const Home = () => {
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight 
         canvas.onmousemove = handleMouseMove
-        camera = {
-            width: .5 * window.innerWidth,
-            height: .5 * window.innerHeight,
-            x: .5 * window.innerWidth / 2,
-            y: .5 * window.innerHeight / 2,
-        }
         
         imageData.forEach(i =>  {
             const image = new Image()
@@ -106,54 +112,164 @@ const Home = () => {
             imagesParam.push({"image": image, "imageX": randX, "imageY": randY, "w": w, "h": h})          
         })      
 
-        const FRAMES_PER_SECOND = 60;  // Valid values are 60,30,20,15,10
-        // set the mim time to render the next frame
-        const FRAME_MIN_TIME = (1000/60) * (60 / FRAMES_PER_SECOND) - (1000/60) * 0.5;
-        let lastFrameTime = 0;  // the last frame time
-
-        const animate = (time) => {
-            draw(imagesParam)
-            if(time-lastFrameTime < FRAME_MIN_TIME){ //skip the frame if the call is to early
-                window.requestAnimFrame(animate);
-                return; // return as there is nothing to do
+        let displayTransform = {
+            x:0,
+            y:0,
+            ox:0,
+            oy:0,
+            scale:1,
+            rotate:0,
+            cx:0,  // chase values Hold the actual display
+            cy:0,
+            cox:0,
+            coy:0,
+            cscale:1,
+            crotate:0,
+            dx:0,  // deltat values
+            dy:0,
+            dox:0,
+            doy:0,
+            dscale:1,
+            drotate:0,
+            drag:0.1,  // drag for movements
+            accel:0.7, // acceleration
+            matrix:[0,0,0,0,0,0], // main matrix
+            invMatrix:[0,0,0,0,0,0], // invers matrix;
+            mouseX:0,
+            mouseY:0,
+            ctx:context,
+            setTransform:function(){
+                var m = this.matrix;
+                var i = 0;
+                this.ctx.setTransform(m[i++],m[i++],m[i++],m[i++],m[i++],m[i++])
+            },
+            setHome:function(){
+                this.ctx.setTransform(1,0,0,1,0,0)
+                
+            },
+            update:function(){
+                // smooth all movement out. drag and accel control how this moves
+                // acceleration 
+                this.dx += (this.x-this.cx)*this.accel
+                this.dy += (this.y-this.cy)*this.accel
+                this.dox += (this.ox-this.cox)*this.accel
+                this.doy += (this.oy-this.coy)*this.accel
+                this.dscale += (this.scale-this.cscale)*this.accel
+                this.drotate += (this.rotate-this.crotate)*this.accel
+                // drag
+                this.dx *= this.drag
+                this.dy *= this.drag
+                this.dox *= this.drag
+                this.doy *= this.drag
+                this.dscale *= this.drag
+                this.drotate *= this.drag
+                // set the chase values. Chase chases the requiered values
+                this.cx += this.dx
+                this.cy += this.dy
+                this.cox += this.dox
+                this.coy += this.doy
+                this.cscale += this.dscale
+                this.crotate += this.drotate
+                
+                // create the display matrix
+                this.matrix[0] = Math.cos(this.crotate)*this.cscale
+                this.matrix[1] = Math.sin(this.crotate)*this.cscale
+                this.matrix[2] =  - this.matrix[1]
+                this.matrix[3] = this.matrix[0];
+        
+                // set the coords relative to the origin
+                this.matrix[4] = -(this.cx * this.matrix[0] + this.cy * this.matrix[2])+this.cox
+                this.matrix[5] = -(this.cx * this.matrix[1] + this.cy * this.matrix[3])+this.coy        
+        
+        
+                // create invers matrix
+                var det = (this.matrix[0] * this.matrix[3] - this.matrix[1] * this.matrix[2])
+                this.invMatrix[0] = this.matrix[3] / det
+                this.invMatrix[1] =  - this.matrix[1] / det
+                this.invMatrix[2] =  - this.matrix[2] / det
+                this.invMatrix[3] = this.matrix[0] / det
+                
+                // check for mouse. Do controls and get real position of mouse.
+                if(mouse !== undefined){  // if there is a mouse get the real cavas coordinates of the mouse
+                    if(mouse.oldX !== undefined){ // check if panning (middle button)
+                        var mdx = mouse.x-mouse.oldX // get the mouse movement
+                        var mdy = mouse.y-mouse.oldY
+                        // get the movement in real space
+                        var mrx = (mdx * this.invMatrix[0] + mdy * this.invMatrix[2])
+                        var mry = (mdx * this.invMatrix[1] + mdy * this.invMatrix[3])  
+                        this.x += mrx / 2
+                        this.y += mry / 2
+                    }
+                    // do the zoom with mouse wheel
+                    if(mouse.w !== undefined && mouse.w !== 0){
+                        this.ox = mouse.x
+                        this.oy = mouse.y
+                        this.x = this.mouseX
+                        this.y = this.mouseY
+                        /* Special note from answer */
+                        // comment out the following is you change drag and accel
+                        // and the zoom does not feel right (lagging and not 
+                        // zooming around the mouse 
+                        /*
+                        this.cox = mouse.x;
+                        this.coy = mouse.y;
+                        this.cx = this.mouseX;
+                        this.cy = this.mouseY;
+                        */
+                        if(mouse.w > 0){ // zoom in
+                            this.scale *= 1.1
+                            mouse.w -= 20
+                            if(mouse.w < 0){
+                                mouse.w = 0
+                            }
+                        }
+                        if(mouse.w < 0){ // zoom out
+                            this.scale *= 1/1.1
+                            mouse.w += 20;
+                            if(mouse.w > 0){
+                                mouse.w = 0
+                            }
+                        }
+        
+                    }
+                    // get the real mouse position 
+                    var screenX = (mouse.x - this.cox)
+                    var screenY = (mouse.y - this.coy)
+                    this.mouseX = this.cx + (screenX * this.invMatrix[0] + screenY * this.invMatrix[2])
+                    this.mouseY = this.cy + (screenX * this.invMatrix[1] + screenY * this.invMatrix[3])            
+                    mouse.rx = this.mouseX  // add the coordinates to the mouse. r is for real
+                    mouse.ry = this.mouseY
+                    // save old mouse position
+                    mouse.oldX = mouse.x
+                    mouse.oldY = mouse.y
+                }
+                
             }
-            lastFrameTime = time; // remember the time of the rendered frame
-            // render the frame
-            window.requestAnimFrame(animate);
+        }
+        let timer = 0
+        const animate = () => {
+            timer += 1; // update timere
+            // update the transform
+            displayTransform.update()
+            // set home transform to clear the screem
+            displayTransform.setHome()
+            context.clearRect(0, 0 ,canvas.width, canvas.height)
+            displayTransform.setTransform()
+            draw(imagesParam)
+
+            requestAnimationFrame(animate)
         }
         animate()
     }, [])
 
-    window.requestAnimFrame = (function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-    })();
-
     const handleMouseMove = (e) => {
-        mouseX = e.offsetX
-        mouseY = e.offsetY
-        moveCamera(mouseX, mouseY)
+        mouse.x = e.offsetX
+        mouse.y = e.offsetY
+        if (mouse.x === undefined) {
+            mouse.x = e.clientX;
+            mouse.y = e.clientY;
+        }
     }
-
-    const moveCamera = (posX, posY) => {
-        camera.x = posX
-        camera.y = posY
-        moveContext()
-    }
-
-    const moveContext = () => {
-        context.save()
-        context.setTransform(1, 0, 0, 1, 0, 0); // Reset context
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.translate(
-            (canvas.width / 5 - camera.x + camera.width / 5) * .4,
-            (canvas.height / 5 - camera.y + camera.height / 5) * .4
-        );
-    }
-
-    let scale=1.00;
-    let scaleDirection=0.01;
 
     const draw = (imagesParam) => {
         imagesParam.forEach(iParam => {
@@ -163,10 +279,10 @@ const Home = () => {
         })
 		
       	// Draw viewport background
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height)
         imagesParam.forEach(iParam => {
-            if(checkDistance(mouseX, iParam.imageX, mouseY, iParam.imageY, iParam.w, iParam.h)) {                
-                context.drawImage(iParam.image, iParam.imageX, iParam.imageY, iParam.w, iParam.h)
+            if(checkDistance(mouse.x, iParam.imageX, mouse.y, iParam.imageY, iParam.w, iParam.h)) {                
+                context.drawImage(iParam.image, iParam.imageX, iParam.imageY, iParam.w * 2, iParam.h * 2)
             } else {
                 context.drawImage(iParam.image, iParam.imageX, iParam.imageY, iParam.w, iParam.h)
             }
